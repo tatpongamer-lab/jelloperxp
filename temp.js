f5dd9a2ab8df0aa371d76d71bcbc4d57d8ec8f32
tatpon <tatpongamer@gmail.com>
2026-08-14 13:03:59 +0700

    ผ้าเช็ดหน้า

---
+++ temp.js
@@ -0,0 +1,19 @@
+
+    tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    navy: '#1a2634',
+                    cream: '#fdfbf7',
+                    orange: '#e05a00',
+                    charcoal: '#333333',
+                    beige: '#e8e1d5'
+                },
+                fontFamily: {
+                    heading: ['"Noto Serif Thai"', 'serif'],
+                    body: ['"Prompt"', 'sans-serif']
+                }
+            }
+        }
+    }
+    
